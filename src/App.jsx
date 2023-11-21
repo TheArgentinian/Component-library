000ea2196f3ee02c0d662b05840d89f7dfb4773d
Federico <fede4488@gmail.com>
2023-11-21 12:25:50 -0300

    menu components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Menu from './src/components/Menu/Menu'
 import Badge from './components/badges'
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
 
   return (
     <>
+    <Menu>
+    <Menu.Button>Menu</Menu.Button>
+    <MenuDropdown/>
+    </Menu>
       <Badge />
     </>
   )
---
+++ src/components/Menu/menu.jsx
@@ -0,0 +1,10 @@
+export default function Menu({children}){
+const [open, setOpen] = React.useState(true)
+
+function toggle(){
+    setOpen(prevOpen => !prevOpen)
+}
+
+
+
+}  
